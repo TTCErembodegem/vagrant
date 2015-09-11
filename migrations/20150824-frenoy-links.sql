bdccae613d7b03b99d3d9d4bd24d524184376bb1
Laoujin <woutervs@hotmail.com>
2015-09-11 19:38:51 +0200

    sql season 2015-2016

--- migrations/20150824-frenoy-links.sql
+++ migrations/20150824-frenoy-links.sql
@@ -3,3 +3,17 @@ VALUES ('Frenoy_ClubId', 'OLV134', 'Frenoy ClubId');
 
 INSERT INTO `ttc_erembodegem`.`parameter` (`sleutel`, `value`, `omschrijving`) 
 VALUES ('Frenoy_SeasonId', '16', 'Huidig Frenoy seizoen');
+
+ALTER TABLE `reeks`
+	ADD COLUMN `FrenoyTeamId` VARCHAR(10) NULL AFTER `LinkID`,
+	ADD COLUMN `FrenoyDivisionId` INT NULL AFTER `FrenoyTeamId`;
+
+ALTER TABLE `kalender`
+	ADD COLUMN `FrenoyMatchId` VARCHAR(20) NULL AFTER `GeleideTraining`;
+
+INSERT INTO `club` (`ID`, `Naam`, `CodeVTTL`, `Website`)
+VALUES (66, 'TTC Het Netje Over Gent', 'OVL102', 'http://www.hetnetjeover.be/');
+
+INSERT INTO `clublokaal` (`ClubID`, `Lokaal`, `Adres`, `Gemeente`, `Postcode`, `Telefoon`)
+VALUES (66, 'Odisee', 'Gebroeders Desmetstraat, 1', 'Gent', 9000, '0477/51 27 56');
+
