0693ce099f454623d61843f11077911d99aa51a1
Laoujin <woutervs@hotmail.com>
2016-02-13 02:48:13 +0100

    cleanup before migration

--- migrations/20160120-manipulations-web-api.sql
+++ migrations/20160120-manipulations-web-api.sql
@@ -6,3 +6,15 @@ update reeks set frenoyteamid=0, frenoydivisionid=0 where frenoyteamid is null;
 UPDATE `ttc_erembodegem`.`speler` SET `Toegang`=1 WHERE  `ID`=85;
 
 DELETE FROM `ttc_erembodegem`.`verslagspeler`;
+
+update club set Naam='Sint Niklase' where Naam='Sint Niklase vzw';
+update club set Naam='Meteor' where Naam='Meteor Oudenaarde';
+update club set Naam='Wielsbeke' where Naam='Wielsbeke TTC';
+update club set Naam='Sterke Stut' where Naam='Sterke Stut Oosterzele';
+update club set Naam='Wetteren' where Naam='De Orchidee Wetteren';
+update club set Naam='Buggenhout' where Naam='Eendracht Buggenhout';
+update club set Naam='Atelia Gent' where Naam='Atelia Gent vzw';
+update club set Naam='Denderleeuw' where Naam='Denderleeuw 2001';
+
+
+delete from kalender where id in (1508, 1499);
